b36d0ea3ff4d0101b0a962121a0bcffe21313461
Bruno Barreiras <brunooliver368@gmail.com>
2024-11-24 21:48:31 -0300

    chore: add cors setup

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -6,6 +6,12 @@ import {
   updateNewPost,
   uploadImage,
 } from "../controllers/postsControllers.js";
+import cors from "cors";
+
+const corsOpt = {
+  origin: "http://localhost:3000",
+  optionsSucessStatus: 200,
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -20,6 +26,7 @@ const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   app.use(express.json());
+  app.use(cors(corsOpt));
 
   app.get("/posts", listPosts);
   app.post("/posts", newPost);
